1c8a8a4f22abaf6baa24a41c7a61c784ef749202
NefariousDarius <126182512+NefariousDarius@users.noreply.github.com>
2023-02-24 13:15:09 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,32 @@
 input.onButtonPressed(Button.A, function () {
-	
+    for (let index = 0; index < 2; index++) {
+        music.setVolume(127)
+        music.setTempo(119)
+        music.playTone(440, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(440, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(220, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(220, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(220, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(220, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(392, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(392, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(196, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+        music.playTone(196, music.beat(BeatFraction.Quarter))
+        basic.pause(115)
+    }
 })
 basic.forever(function () {
     basic.showLeds(`
